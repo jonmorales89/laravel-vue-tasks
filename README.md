c930f163a751273b33aea4baf38ab15f0fa0e882
jonmorales89 <jon.sal.morales@gmail.com>
2017-11-13 00:36:56 -0800

    updated readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Task List App
+Developed using Laravel and Vue JS
+
+Simple app that utilizes Create Read and Delete fucntionalities
+
+App interactes with MySQL database
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('api')->group(/**
+ *
+ */
+    function () {
+    Route::resource('tasks', 'TaskController');
+});
